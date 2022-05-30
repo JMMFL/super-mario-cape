ed607b77f64bc5f0e7d6929bca54cb926e438d92
JMMFL <mehravarj+github@gmail.com>
2022-05-30 16:13:24 -0400

    Created overworldlevel with legacy code.

--- modules/levels/overworld.js
+++ modules/levels/overworld.js
@@ -1,14 +1,12 @@
-import { SCREEN } from "../../main.js";
 import SPAWNER from "../spawner.js";
 import Level from "../classes/Level.js";
-import YellowParakoopa from "../classes/YellowParakoopa.js";
-import RedSuperKoopa from "../classes/RedSuperKoopa.js";
-import YellowSuperKoopa from "../classes/YellowSuperKoopa.js";
 import FlyingGoomba from "../classes/FlyingGoomba.js";
+import SuperKoopa from "../classes/legacy/SuperKoopa.js";
+import Parakoopa from "../classes/legacy/Parakoopa.js";
 import Star from "../classes/Star.js";
 
 const OVERWORLD = new Level(
-  "Creative Name",
+  "The Basics",
   "overworld",
   "overworld",
   0.5,
@@ -16,21 +14,61 @@ const OVERWORLD = new Level(
 );
 
 function spawnFn() {
-  SPAWNER.makeCol(YellowSuperKoopa, 3, 0, 1);
-  SPAWNER.makeOne(RedSuperKoopa, 100, 2.5);
-  SPAWNER.makeOne(RedSuperKoopa, 600, 2.5);
-  SPAWNER.makeOne(YellowParakoopa, 400, 5);
-  SPAWNER.makeOne(FlyingGoomba, 250, 6);
-  SPAWNER.makeRow(YellowParakoopa, 6, 350, 8);
-  SPAWNER.makeCol(FlyingGoomba, 5, 0, 11);
-  SPAWNER.makeCol(FlyingGoomba, 5, 300, 13);
-  SPAWNER.makeRow(YellowSuperKoopa, 5, 100, 16);
-  SPAWNER.makeRow(FlyingGoomba, 3, 625, 18);
-  SPAWNER.makeRow(FlyingGoomba, 2, 200, 18.5);
-  SPAWNER.makeRow(RedSuperKoopa, 11, 100, 18.5);
-  SPAWNER.makeRow(YellowParakoopa, 3, 100, 22);
-  SPAWNER.makeRow(YellowParakoopa, 4, 500, 24.5);
-  SPAWNER.makeOne(Star, SCREEN.centerY, 26);
+  const { spawnElement } = SPAWNER;
+
+  spawnElement(1, new SuperKoopa(0, "yellow"));
+  spawnElement(1, new SuperKoopa(100, "yellow"));
+  spawnElement(1, new SuperKoopa(200, "yellow"));
+  spawnElement(2.5, new SuperKoopa(600, "red"));
+  spawnElement(2.5, new SuperKoopa(100, "red"));
+  spawnElement(4, new Parakoopa(300, "red"));
+  spawnElement(5, new Parakoopa(400, "yellow"));
+  spawnElement(6, new FlyingGoomba(250));
+  spawnElement(8, new Parakoopa(350, "yellow"));
+  spawnElement(8.5, new Parakoopa(300, "yellow"));
+  spawnElement(9, new Parakoopa(300, "yellow"));
+  spawnElement(9.5, new Parakoopa(300, "yellow"));
+  spawnElement(10, new Parakoopa(300, "yellow"));
+  spawnElement(10.5, new Parakoopa(300, "yellow"));
+  spawnElement(11, new FlyingGoomba(0));
+  spawnElement(11, new FlyingGoomba(100));
+  spawnElement(11, new FlyingGoomba(200));
+  spawnElement(11, new FlyingGoomba(300));
+  spawnElement(11, new FlyingGoomba(400));
+  spawnElement(13, new FlyingGoomba(250));
+  spawnElement(13, new FlyingGoomba(350));
+  spawnElement(13, new FlyingGoomba(450));
+  spawnElement(13, new FlyingGoomba(550));
+  spawnElement(13, new FlyingGoomba(650));
+  spawnElement(16, new SuperKoopa(100, "yellow"));
+  spawnElement(16.25, new SuperKoopa(100, "yellow"));
+  spawnElement(16.5, new SuperKoopa(100, "yellow"));
+  spawnElement(16.75, new SuperKoopa(100, "yellow"));
+  spawnElement(17, new SuperKoopa(100, "yellow"));
+  spawnElement(18.5, new FlyingGoomba(200));
+  spawnElement(19.5, new FlyingGoomba(200));
+  spawnElement(18, new FlyingGoomba(625));
+  spawnElement(19, new FlyingGoomba(625));
+  spawnElement(20, new FlyingGoomba(625));
+  spawnElement(18.5, new SuperKoopa(100, "red"));
+  spawnElement(18.75, new SuperKoopa(100, "red"));
+  spawnElement(19, new SuperKoopa(100, "red"));
+  spawnElement(19.25, new SuperKoopa(100, "red"));
+  spawnElement(19.5, new SuperKoopa(100, "red"));
+  spawnElement(19.75, new SuperKoopa(100, "red"));
+  spawnElement(20, new SuperKoopa(100, "red"));
+  spawnElement(20.25, new SuperKoopa(100, "red"));
+  spawnElement(20.5, new SuperKoopa(100, "red"));
+  spawnElement(20.75, new SuperKoopa(100, "red"));
+  spawnElement(21, new SuperKoopa(100, "red"));
+  spawnElement(22, new Parakoopa(100, "yellow"));
+  spawnElement(22.5, new Parakoopa(100, "yellow"));
+  spawnElement(23, new Parakoopa(100, "yellow"));
+  spawnElement(24.5, new Parakoopa(500, "red"));
+  spawnElement(25, new Parakoopa(500, "red"));
+  spawnElement(25.5, new Parakoopa(500, "red"));
+  spawnElement(26, new Parakoopa(500, "red"));
+  spawnElement(27, new Star());
 }
 
 export default OVERWORLD;
